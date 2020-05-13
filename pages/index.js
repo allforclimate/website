35f39e09e753c241be2af34d94ef97d10ae699ec
Xavier Damman <xdamman@gmail.com>
2020-05-13 14:57:16 +0200

    updated homepage

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,86 @@
 import styled from 'styled-components';
 
-const Links = styled.ul`
-  list-style: none;
+const Body = styled.div`
+  max-width: 660px;
+  margin: 0 auto;
+  font-family: Content-font, Roboto, sans-serif;
+  font-weight: 400;
+  line-height: 1.625;
+  font-size: 16px;
 `;
 
-const Link = styled.li`
+const Links = styled.div`
+  padding:0;
+  margin:0;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  display: block;
   margin: 15px 0;
   border: 1px solid #4A7A84;
   border-radius: 5px;
+  min-width: 250px;
   max-width: 300px;
-  padding: 5px;
-  font-family: Helvetica, Arial;
+  padding: 10px;
+  &:hover {
+    background: #eee
+  }
 `;
 
-const A = styled.a`
+const About = styled.div`
+  margin: 50px 0;
+`;
+
+const Footer = styled.div`
+
+`;
+
+const FooterLink = styled.a`
   text-decoration: none;
+  margin-right: 20px;
+  font-size: 11pt;
 `;
 
+const Label = styled.span`
+  color: #555;
+  margin-right: 20px;
+`;
+
+const H2 = styled.h2`
+  font-size: 24px;
+  font-weight: 700;
+  margin: 40px 0 10px;
+`;
 
 export default () => (
-  <div>
+  <Body>
     <center>
       <img src="/images/allforclimate-logo.png" />
       <Links>
-        <Link><A href="https://docs.allforclimate.earth">📕 Documentation</A></Link>
-        <Link><A href="https://drive.allforclimate.earth">💻 Drive</A></Link>
-        <Link><A href="https://opencollective.com/allforclimate">🧾 Open Collective</A></Link>
-        <Link><A href="https://github.com/allforclimate">👩🏻‍💻 GitHub</A></Link>
-        <Link><A href="https://facebook.com/allforclimate">👨‍👩‍👧‍👦 Facebook</A></Link>
+        <Link href="https://opencollective.com/allforclimate">List of collectives we are hosting</Link>
+        <Link href="https://opencollective.com/allforclimate/apply">Apply to host your collective</Link>
+        <Link href="https://docs.allforclimate.earth">📕 Documentation</Link>
+        <Link href="https://drive.allforclimate.earth">📂 Google Drive</Link>
       </Links>
+
+      <About>
+        <H2>A fiscal sponsor for your local climate justice group</H2>
+        <p>As a local group you probably don't have the resources to create your own non profit with your own bank account. You also probably don't want to use your personal bank account to receive donations and manage the expenses for your group. We have created a non profit based in Brussels that can act as a fiscal sponsor for you (in the eurozone), think of it as your financial back office. <a href="https://docs.allforclimate.earth/services/fiscal-hosting">More info</a>.</p>
+
+        <H2>An easy way for foundations to support local climate justice groups</H2>
+        <p>Local groups don't usually have a bank account where you can wire money to, nor do they have the administrative resources to do accounting and to write the necessary reports you need. All for Climate is set up as a proper non profit entity based in Brussels that you can trust. We act as a shared administrative back office so that local groups can focus on their actions. <a href="mailto:info@allforclimate.earth">Contact us</a>.</p>
+      </About>
+
+      <Footer>
+        <Label>Find us on:</Label>
+        <FooterLink href="https://opencollective.com/allforclimate">Open Collective</FooterLink>
+        <FooterLink href="https://github.com/allforclimate">GitHub</FooterLink>
+        <FooterLink href="https://facebook.com/allforclimate">Facebook</FooterLink>
+        <FooterLink href="mailto:info@allforclimate.earth">Email</FooterLink>
+        <br />
+        <Label>All for Climate ASBL, Brussels</Label>
+      </Footer>
     </center>
-  </div>
+  </Body>
 )
