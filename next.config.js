dfed6265465addf5dbaa30ae6e02cf5f16d5149f
Daniel Sum <daniel@lacerisenumerique.be>
2020-10-01 22:22:17 +0200

    Add a transparency page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   env: {
-    OC_GRAPHQL_API: 'https://api.opencollective.com/graphql/v1/'
+    OC_GRAPHQL_API: 'https://api.opencollective.com/graphql/v1/',
+    OC_GRAPHQL_API_V2: 'https://api.opencollective.com/graphql/v2/',
+    OC_GRAPHQL_API_KEY: ''
   }
 };
