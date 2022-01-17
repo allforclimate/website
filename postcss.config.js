084bb7a93e5281376be2cde661b1348940afb341
Xavier Damman <xdamman@gmail.com>
2022-01-17 13:01:46 +0100

    upgrade purgecss and sitemap /workgroups

--- postcss.config.js
+++ postcss.config.js
@@ -1,34 +1,61 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    // Specify the paths to all of the template files
-    content: [
-      "./pages/**/*.{js,jsx,ts,tsx}",
-      "./components/**/*.{js,jsx,ts,tsx}",
-    ],
-    // This is the function used to extract class names from the templates
-    defaultExtractor: (content) => {
-      const arr = content.match(/[\w-/:]+(?<!:)/g) || [];
-      arr.push(
-        "ul",
-        "li",
-        "pagebreak",
-        "p",
-        "h1",
-        "h2",
-        "h3",
-        "h4",
-        "h5",
-        "h6"
-      );
-      return arr;
-    },
-  },
-];
 module.exports = {
   plugins: [
     "tailwindcss",
-    process.env.NODE_ENV === "production" ? purgecss : undefined,
-    "postcss-preset-env",
+    [
+      "@fullhuman/postcss-purgecss",
+      process.env.NODE_ENV === "production"
+        ? {
+            // the paths to all template files
+            content: [
+              "./pages/**/*.{js,jsx,ts,tsx}",
+              "./components/**/*.{js,jsx,ts,tsx}",
+            ],
+            // function used to extract class names from the templates
+            defaultExtractor: (content) => {
+              const arr = content.match(/[\w-/:]+(?<!:)/g) || [];
+              arr.push(
+                "ul",
+                "li",
+                "pagebreak",
+                "p",
+                "h1",
+                "h2",
+                "h3",
+                "h4",
+                "h5",
+                "h6"
+              );
+              return arr;
+            },
+          }
+        : false,
+    ],
   ],
 };
+
+// const purgecss = require("@fullhuman/postcss-purgecss")({
+//   // paths to all of the template files in the project
+//   content: [
+//     "./pages/**/*.{js,jsx,ts,tsx}",
+//     "./components/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   // This is the function used to extract class names from the templates
+//   defaultExtractor: (content) => {
+//     const arr = content.match(/[\w-/:]+(?<!:)/g) || [];
+//     arr.push("ul", "li", "pagebreak", "p", "h1", "h2", "h3", "h4", "h5", "h6");
+//     return arr;
+//   },
+// });
+
+// module.exports = {
+//   plugins: [
+//     require("tailwindcss"),
+//     require("postcss-preset-env")({
+//       stage: 1,
+//       features: {
+//         "focus-within-pseudo-class": false,
+//       },
+//     }),
+//     ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+//   ],
+// };
